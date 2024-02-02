69e95a1923feef96a2a7d82028cf1b3094513e8c
Owais Shariff <osh.econnect@gmail.com>
2024-02-03 02:13:27 +0530

    Update navbar.ts

--- src/lib/content/navbar.ts
+++ src/lib/content/navbar.ts
@@ -4,7 +4,7 @@ export const navbarSection: NavbarSectionType = {
     navLinks: [
         { name: 'chart', url: 'https://birdeye.so/token/3VPirK8MvNsRL26ABnFxgERvBBcNzzcqXjcMMp9MD2VD?chain=solana'},
         { name: 'telegram', url: 'https://t.me/ChickenSol'},
-        { name: 'x/twitter', url: '#' },
+        { name: 'x/twitter', url: 'https://x.com/Chikensol' },
     ],
     cta: {
         title: 'buy now',
